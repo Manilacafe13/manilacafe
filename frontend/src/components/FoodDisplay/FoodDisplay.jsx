96d0d4c40a46fd702d5967f4d9eca41e7519fae4
Manilacafe13 <manilacafe.goteborg@gmail.com>
2026-09-05 00:13:07 +0200

    Improve product section SEO

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,22 +3,61 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = ({category}) => {
+const FoodDisplay = ({ category }) => {
 
-  const {food_list} = useContext(StoreContext)
+  const { food_list } = useContext(StoreContext)
 
   return (
-    <div className='food-display' id='food-display'>
-     
-     <div className="food-display-list">
-        {food_list.map((item,index)=>{
-          if (category==="All" || category===item.category) {
-          return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
- 
+    <section
+      className='food-display'
+      id='food-display'
+      aria-labelledby='food-display-title'
+    >
+
+      <div className='food-display-intro'>
+
+        <h2 id='food-display-title'>
+          Filippinska desserter i Göteborg
+        </h2>
+
+        <p>
+          Upptäck Manila Cafés utbud av filippinska desserter,
+          med favoriter som Mango Float, Ube Cake, Taho,
+          Fruit Salad och andra tropiska smaker.
+        </p>
+
+      </div>
+
+
+      <div className='food-display-list'>
+
+        {food_list.map((item) => {
+
+          if (
+            category === "All" ||
+            category === item.category
+          ) {
+
+            return (
+              <FoodItem
+                key={item._id}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            )
+
           }
+
+          return null
+
         })}
-     </div>
-    </div>
+
+      </div>
+
+    </section>
   )
 }
 
